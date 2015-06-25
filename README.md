a9e1a07b253fe9c47a28b7706250c0da9d31bd0d
José María Fernández <jmfernandez@cnio.es>
2015-06-25 23:07:09 +0200

    Added first untested version of the program. E-mail body is not pamaterized yet

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# rd-connect-password-emailer
-RD-Connect scripts to batch create users from a tabular file with e-mail, name, and possibly preferred username
+# RD-Connect password emailer
+RD-Connect Perl scripts to batch create users from a tabular file with e-mail, name, and possibly preferred username.
+
+This program uses next Perl modules:
+
+* Config::IniFiles
+* DBI and the database modules needed (DBD::SQLite)
+* Digest::SHA1 (or the corresponding module to encode passwords)
+* Email::Address
+* Email::MIME
+* Email::Sender::Transport::SMTPS
+* MooX::Types::MooseLike (Email::Sender::Transport::SMTPS depends on it but it does not declare this dependency).
+
+and [APG](http://www.adel.nursat.kz/apg/ "Another Password Generator") program (which must be either declared
+in the configuration profile or it must be found in the `PATH`).
+
+```sql
+CREATE TABLE users (
+    username varchar(50) not null,
+    password varchar(50) not null,
+    fullname varchar(4096) not null,
+    email varchar(64) not null,
+    active boolean not null,
+    primary key(username)
+);
+```
---
+++ create-rd-connect-users.pl
@@ -0,0 +1,159 @@
+#!/usr/bin/perl
+
+use warnings "all";
+use strict;
+
+use Carp;
+use Config::IniFiles;
+use DBI;
+use Digest;
+use Email::Address;
+use Email::MIME;
+use Email::Sender::Simple qw(sendmail);
+use Email::Sender::Transport::SMTPS qw();
+
+use constant SECTION	=>	'main';
+use constant MAILSECTION	=>	'mail';
+use constant APGSECTION	=>	'apg';
+
+my $doReplace;
+if(scalar(@ARGV)>0 && $ARGV[0] eq '-r') {
+	shift(@ARGV);
+	$doReplace = 1;
+}
+
+if(scalar(@ARGV)>=2) {
+	my $configFile = shift(@ARGV);
+	my $cfg = Config::IniFiles->new( -file => $configFile);
+	
+	# Now, let's read all the parameters
+	
+	# The database connection parameters
+	my $dbistr = $cfg->val(SECTION,'dbistr');
+	Carp::croak("dbistr parameter was not defined in $configFile")  unless(defined($dbistr));
+	
+	my $dbuser = $cfg->val(SECTION,'dbuser','');
+	my $dbpass = $cfg->val(SECTION,'dbpass','');
+	
+	# Database connection
+	my $conn = DBI->connect($dbistr,$dbuser,$dbpass,{ RaiseError => 0, AutoCommit => ($doReplace?1:0)});
+	
+	# Preparing the sentences
+	my $insertStr = 'INSERT';
+	if($doReplace) {
+		print "NOTICE: Replace mode enabled\n";
+		$insertStr .= ' OR REPLACE';
+	}
+	$insertStr .= ' INTO USERS VALUES (?,\'\',?,?,0)';
+	my $insSth = $conn->prepare($insertStr);
+	
+	my $updSth = $conn->prepare('UPDATE USERS SET password=? , active=1 WHERE username=?');
+	
+	# The digest algorithm
+	my $digestAlg = $cfg->val(SECTION,'digest','SHA-1');
+	my $digest = Digest->new($digestAlg);
+	
+	# apg path
+	my $apgPath = $cfg->val(APGSECTION,'apgPath','apg');
+	my $apgMin = $cfg->val(APGSECTION,'min-length',12);
+	my $apgMax = $cfg->val(APGSECTION,'max-length',16);
+	
+	my @apgParams = ($apgPath,'-m',$apgMin,'-x',$apgMax,'-n',1,'-k','-q');
+	
+	# Mail configuration parameters
+	my @mailParams = ();
+	foreach my $mailParam ('host','ssl','port','sasl_username','sasl_password') {
+		push(@mailParams,$mailParam,$cfg->val(MAILSECTION,$mailParam))  if($cfg->exists(MAILSECTION,$mailParam));
+	}
+	my $transport = Email::Sender::Transport::SMTPS->new(@mailParams);
+	
+	my $from = Email::Address->parse($cfg->val(MAILSECTION,'from'));
+	
+	Carp::croak("subject field must be defined in order to send e-mails")  unless($cfg->exists(MAILSECTION,'subject'));
+	my $subject = $cfg->val(MAILSECTION,'subject');
+	
+	my $usersFile = shift(@ARGV);
+	if(open(my $U,'<:encoding(UTF-8)',$usersFile)) {
+		while(my $line=<$U>) {
+			chomp($line);
+			my($email,$fullname,$username,$junk) = split(/\t/,$line,4);
+			my @addresses = Email::Address->parse($email);
+			
+			if(scalar(@addresses)==0) {
+				Carp::carp("Unable to parse e-mail $email from user $fullname. Skipping");
+			} else {
+				# The destination user
+				my $to = $addresses[0];
+				$to->phrase($fullname)  unless(defined($fullname) && length($fullname)>0 && !defined($to->phrase));
+				
+				# Re-defining the values based on the parse step
+				# Defining an username
+				$username = $to->address()  unless(defined($username) && length($username)>0);
+				
+				# Re-defining the fullname
+				$fullname = $to->phrase();
+				
+				# Re-defining the e-mail
+				$email = $to->address();
+				
+				# Now, let's read the generated password
+				if(open(my $APG,'-|',@apgParams)) {
+					my $pass = <$APG>;
+					chomp($pass);
+					
+					# Setting the digester to a known state
+					$digest->reset();
+					$digest->add($pass);
+					my $digestedPass = $digest->hexdigest;
+					if($insSth->execute($username,$fullname,$email) || $doReplace) {
+						if($updSth->execute($digestedPass,$username)) {
+							# Now, committing changes
+							$conn->commit()  unless(defined($doReplace));
+							
+							# Preparing and sending the e-mail
+							my $message = Email::MIME->create(
+								header_str => [
+									From	=>	$from,
+									To	=>	$to,
+									Subject	=>	$subject
+								],
+								attributes => {
+									encoding => 'quoted-printable',
+									charset  => 'ISO-8859-1',
+								},
+								body_str => <<EOF
+Dear $fullname,
+	your username for RD-Connect platform is $username, and the automatically generated password is $pass.
+	
+	You should change this password by a different one as soon as possible.
+
+	Kind Regards,
+		RD-Connect team
+EOF
+							);
+							eval {
+								sendmail($message, { transport => $transport });
+							};
+							
+							if($@) {
+								Carp::croak("Error while sending e-mail: ",$@);
+							}
+						}
+					} else {
+						# Reverting state
+						Carp::carp("Unable to add user $username (fullname $fullname, e-mail $email). Maybe does it already exist?");
+						$conn->rollback();
+					}
+				} else {
+					Carp::croak("Unable to generate a password using apg\n");
+				}
+			}
+		}
+		close($U);
+	} else {
+		Carp::croap("Unable to read file $usersFile");
+	}
+	
+} else {
+	die "Usage: $0 [-r] {IniFile} {Tabular file with new users (in UTF-8)}"
+}
