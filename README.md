cab51a58c9dca98df565a161dcf34d561466114e
José María Fernández <jmfernandez@cnio.es>
2015-07-08 21:27:41 +0200

    Added templating capabilities, as well as attachment inclusion.

--- README.md
+++ README.md
@@ -9,7 +9,10 @@ This program uses next Perl modules:
 * Email::Address
 * Email::MIME
 * Email::Sender::Transport::SMTPS
+* File::Basename
+* File::MimeInfo
 * MooX::Types::MooseLike (Email::Sender::Transport::SMTPS depends on it but it does not declare this dependency).
+* Text::Unidecode
 
 and [APG](http://www.adel.nursat.kz/apg/ "Another Password Generator") program (which must be either declared
 in the configuration profile or it must be found in the `PATH`).
@@ -24,3 +27,25 @@ CREATE TABLE users (
     primary key(username)
 );
 ```
+
+# Future developments
+Currently, the e-mails being sent are not neither signed nor encrypted, which is not satisfactory.
+
+So, the plan to move to encrypted and signed e-mails has two steps.
+
+1. In the first phase, all the e-mails the platform sends are going to be signed using OpenPGP standard.
+This requires that all the e-mail addresses sending e-mails have a pair of public/private keys, and their
+public keys published in standard servers.
+
+  People who wish to verify the origin of the e-mails will need to use next e-mail client extensions:
+  * For Mozilla Thunderbird users, you must install [Enigmail](https://www.enigmail.net/) extension.
+  * For Apple Mail users, you must install [GPG Suite](https://gpgtools.org/).
+  * For Microsoft Outlook users, you must install [Ggp4win](http://www.gpg4win.org/).
+  * For K-9 Mail users, you must install [APG](https://play.google.com/store/apps/details?id=org.thialfihar.android.apg).
+
+2. In the second phase, all the e-mails the platform sends are going to be encrypted and signed using
+OpenPGP standard
+
+The tool is also going to migrate the user creation style, from a SQLite database to LDAP.
+Module [Net::LDAP](http://search.cpan.org/~gbarr/perl-ldap/lib/Net/LDAP/Examples.pod) will be used,
+in order to emulate command line managemement seen [in this link](http://www.thegeekstuff.com/2015/02/openldap-add-users-groups/).
--- create-rd-connect-users.pl
+++ create-rd-connect-users.pl
@@ -11,6 +11,9 @@ use Email::Address;
 use Email::MIME;
 use Email::Sender::Simple qw(sendmail);
 use Email::Sender::Transport::SMTPS qw();
+use File::Basename qw();
+use File::MimeInfo::Magic qw();
+use Text::Unidecode qw();
 
 use constant SECTION	=>	'main';
 use constant MAILSECTION	=>	'mail';
@@ -22,8 +25,12 @@ if(scalar(@ARGV)>0 && $ARGV[0] eq '-r') {
 	$doReplace = 1;
 }
 
-if(scalar(@ARGV)>=2) {
+if(scalar(@ARGV)>=3) {
 	my $configFile = shift(@ARGV);
+	my $usersFile = shift(@ARGV);
+	my $mailTemplate = shift(@ARGV);
+	my @attachmentFiles = @ARGV;
+	
 	my $cfg = Config::IniFiles->new( -file => $configFile);
 	
 	# Now, let's read all the parameters
@@ -72,7 +79,60 @@ if(scalar(@ARGV)>=2) {
 	Carp::croak("subject field must be defined in order to send e-mails")  unless($cfg->exists(MAILSECTION,'subject'));
 	my $subject = $cfg->val(MAILSECTION,'subject');
 	
-	my $usersFile = shift(@ARGV);
+	# Read the mail body
+	my $templateMailBodyMime;
+	eval {
+		$templateMailBodyMime = File::MimeInfo::Magic::mimetype($mailTemplate);
+	};
+	Carp::croak("ERROR: Unable to pre-process mail template $mailTemplate")  if($@ || !defined($templateMailBodyMime));
+	
+	my $templateMailBody;
+	if(open(my $TT,'<:utf8',$mailTemplate)) {
+		local $/;
+		$templateMailBody = <$TT>;
+		
+		close($TT);
+	} else {
+		Carp::croak("Unable to read template body");
+	}
+	
+	# These are the recognized replacements
+	my %keyval = ( 'username' => '(undefined)', 'fullname' => '(undefined)' );
+	my %replacements = map { $_ => undef } $templateMailBody =~ /\[% ([a-zA-Z0-9._-]+) %\]/g;
+	foreach my $var (keys(%replacements)) {
+		Carp::carp("WARNING: annotation $var in template does not exist")  unless(exists($keyval{$var}));
+	}
+	
+	# Identify the attachments
+	my @attachments = ();
+	foreach my $attachmentFile (@attachmentFiles) {
+		my $mime;
+		eval {
+			$mime = File::MimeInfo::Magic::mimetype($attachmentFile);
+		};
+		
+		Carp::croak("ERROR: Unable to pre-process attachment $attachmentFile")  if($@ || !defined($mime));
+		
+		if(open(my $AT,'<:raw',$attachmentFile)) {
+			local $/;
+			push(@attachments,Email::MIME->create(
+				attributes => {
+					filename => File::Basename::basename($attachmentFile),
+					content_type => $mime,
+					disposition => "attachment",
+					encoding => "base64",
+					# name => ,
+				},
+				body => <$AT>
+			));
+			
+			close($AT);
+		} else {
+			Carp::croak("ERROR: Unable to read attachment $attachmentFile");
+		}
+	}
+	
+	# Read the users
 	if(open(my $U,'<:encoding(UTF-8)',$usersFile)) {
 		while(my $line=<$U>) {
 			chomp($line);
@@ -88,10 +148,23 @@ if(scalar(@ARGV)>=2) {
 				$email = $to->address();
 				
 				$fullname = $to->phrase  unless(defined($fullname) && length($fullname)>0);
+				# Removing possible spaces before and after the fullname
+				$fullname =~ s/^ +//;
+				$fullname =~ s/ +$//;
 				
-				# Re-defining the values based on the parse step
-				# Defining an username
-				$username = $to->address()  unless(defined($username) && length($username)>0);
+				unless(defined($username) && length($username) > 0) {
+					$username = Text::Unidecode::unidecode($fullname);
+					
+					# Replacing apostrophes and commas by points
+					$username =~ tr/a-zA-Z0-9._-/./c;
+					
+					# Re-defining the values based on the parse step
+					# Defining an username
+					unless(defined($username) && length($username)>0) {
+						$username = $to->address();
+						$username = substr($username,0,index($username,'@'));
+					}
+				}
 				
 				# Re-defining the object
 				$to = Email::Address->new($fullname => $email);
@@ -110,29 +183,65 @@ if(scalar(@ARGV)>=2) {
 							# Now, committing changes
 							$conn->commit()  unless(defined($doReplace));
 							
-							# Preparing and sending the e-mail
+							# Preparing and sending the e-mails
+							my $mailBody = $templateMailBody;
+							
+							# Substitute
+							$keyval{'username'} = $username;
+							$keyval{'fullname'} = $fullname;
+							foreach my $var (keys(%replacements)) {
+								my $val = exists($keyval{$var}) ? $keyval{$var} : '(FIXME!)';
+								$mailBody =~ s/\Q[% $var %]\E/$val/g;
+							}
+							
+							my $mailPart = Email::MIME->create(
+								attributes => {
+									encoding => 'quoted-printable',
+									content_type => $templateMailBodyMime,
+									charset  => 'UTF-8',
+								},
+								body_str => $mailBody
+							);
+							
 							my $message = Email::MIME->create(
 								header_str => [
 									From	=>	$from,
 									To	=>	$to,
-									Subject	=>	$subject
+									Subject	=>	$subject.' (I)'
 								],
 								attributes => {
-									encoding => 'quoted-printable',
-									charset  => 'ISO-8859-1',
+									content_type => 'multipart/mixed'
 								},
-								body_str => <<EOF
-Dear $fullname,
-	your username for RD-Connect platform is $username, and the automatically generated password is $pass.
-	
-	You should change this password by a different one as soon as possible.
-
-	Kind Regards,
-		RD-Connect team
-EOF
+								parts => [ $mailPart, @attachments ]
 							);
 							eval {
 								sendmail($message, { from => $from->address(), transport => $transport });
+								my $passmessage = Email::MIME->create(
+									header_str => [
+										From	=>	$from,
+										To	=>	$to,
+										Subject	=>	$subject.' (II)'
+									],
+									attributes => {
+										encoding => 'quoted-printable',
+										charset  => 'ISO-8859-1',
+									},
+									body_str => <<EOF
+The automatically generated password is $pass.
+
+You should change this password by a different one as soon as possible.
+
+Kind Regards,
+	RD-Connect team
+EOF
+								);
+								eval {
+									sendmail($passmessage, { from => $from->address(), transport => $transport });
+								};
+								
+								if($@) {
+									Carp::croak("Error while sending password e-mail: ",$@);
+								}
 							};
 							
 							if($@) {
@@ -141,7 +250,7 @@ EOF
 						}
 					} else {
 						# Reverting state
-						Carp::carp("Unable to add user $username (fullname $fullname, e-mail $email). Maybe does it already exist?");
+						Carp::carp("Unable to add user $username (fullname $fullname, e-mail $email). Does it already exist, maybe?");
 						$conn->rollback();
 					}
 				} else {
@@ -151,9 +260,9 @@ EOF
 		}
 		close($U);
 	} else {
-		Carp::croap("Unable to read file $usersFile");
+		Carp::croak("Unable to read file $usersFile");
 	}
 	
 } else {
-	die "Usage: $0 [-r] {IniFile} {Tabular file with new users (in UTF-8)}"
+	die "Usage: $0 [-r] {IniFile} {Tabular file with new users (in UTF-8)} {Message Template} {Attachments}*"
 }
