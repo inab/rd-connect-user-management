3f3dcb880755013a04d9efc32c554de151718148
José María Fernández <jmfernandez@cnio.es>
2016-12-20 16:24:41 +0100

    Updated the automated installation method, which was failing on several Perl modules with failing tests

--- install-deps.sh
+++ install-deps.sh
@@ -1,15 +1,31 @@
 #!/bin/sh
 
-(echo y;echo o conf prerequisites_policy follow;echo o conf commit) | cpan
+set -e
+
 export PERL_LOCAL_LIB_ROOT="$PERL_LOCAL_LIB_ROOT:${HOME}/perl5"
 export PERL_MB_OPT="--install_base ${HOME}/perl5"
 export PERL_MM_OPT="INSTALL_BASE=""${HOME}""/perl5"
 export PERL5LIB="${HOME}/perl5/lib/perl5:$PERL5LIB"
 export PATH="${HOME}/perl5/bin:$PATH"
+( echo ; echo ) | cpan
+(echo o conf prerequisites_policy follow;echo o conf commit) | cpan
+# It has a failing test
+cpan -T ExtUtils::MakeMaker
+cpan -i local::lib
+
+
+
 export NO_NETWORK_TESTING=n
-cpan -i LWP LWP::Protocol::https Test::More boolean experimental \
+for PACKA in LWP LWP::Protocol::https Test::More boolean experimental \
 Config::IniFiles Net::LDAP MIME::Base64 Digest::MD5 Digest::SHA1 \
 JSON::Validator Email::Address Email::MIME \
-Email::Sender::Transport::SMTPS Env File::MimeInfo Text::Unidecode && \
-(echo y; echo y) | cpan -i Template && \
-cpan -i Authen::CAS::Client Authen::CAS::External Dancer2 Plack::Middleware::CrossOrigin Plack::Middleware::Deflater FCGI && exit 0
+Email::Sender::Transport::SMTPS Env File::MimeInfo Text::Unidecode ; do
+	cpan -i "$PACKA"
+done
+
+(echo ; echo ) | cpan -i Template
+# It has several failing tests
+cpan -T Moose
+for PACKA in Authen::CAS::Client Authen::CAS::External Dancer2 Plack::Middleware::CrossOrigin Plack::Middleware::Deflater FCGI ; do
+	cpan -i "$PACKA"
+done
